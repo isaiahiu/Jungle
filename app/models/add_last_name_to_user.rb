8117719c785d01408e1043349e0c5464c0d8c7ac
Isaiah <isaiah.iu@hotmail.com>
2022-05-06 00:54:58 +0000

    Changes to Users table, top nav bar partial
    - Create new migration to add column for last_names for users table.
    - Add login and signup links to the top nav bar, which persists on every
    page. When user is logged in the logout link will persist.
    - Ready to merge with master.

---
+++ app/models/add_last_name_to_user.rb
@@ -0,0 +1,2 @@
+class AddLastNameToUser < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_05_222501) do
+ActiveRecord::Schema.define(version: 2022_05_06_001311) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2022_05_05_222501) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "last_name"
   end
 
   add_foreign_key "line_items", "orders"
