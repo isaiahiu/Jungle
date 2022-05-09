c5204954e20d58c121b4a556f27bfba477af345e
Isaiah <isaiah.iu@hotmail.com>
2022-05-09 15:55:41 +0000

    Create Users rspec, validations for valid fields

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+require "user"
+
+RSpec.describe User, type: :model do
+ 
+  describe "Validations"
+  context "All fields are valid" do
+    it "should save successfully" do
+      @user = User.create(name: "John", last_name: "Smith", email: "test@test.com", password: "jungle", password_confirmation: "jungle")
+
+      expect(@user.valid?).to eq true
+      expect(@user.id).to be_present
+    end
+  end
+
+  context "Password and password confirmation dont match" do
+    it "should return an error" do
+      @user = User.create(name: "John", last_name: "Smith", email: "test@test.com", password: "jungle", password_confirmation: "book")
+
+      expect(@user.errors.full_messages).to include "Password confirmation doesn't match Password"
+    end
+  end
+end
