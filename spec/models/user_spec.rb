5fda64f75a6928e20b5bc7e9c6844eac35cc4e32
Isaiah <isaiah.iu@hotmail.com>
2022-05-09 16:05:44 +0000

    Create failing unique email test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -28,4 +28,14 @@ RSpec.describe User, type: :model do
       expect(@user.errors.full_messages).to include "Password can't be blank"
     end
   end
+
+  context "Emails must be unique and not case sensitive" do
+    it "should return an error if email has been used" do
+       @user1 = User.create(name: "John", last_name: "Smith", email: "test@test.com", password: "jungle", password_confirmation: "jungle")
+
+       @user2 = User.create(name: "Copy", last_name: "OfJohn", email: "test@TEST.com", password: "jungle", password_confirmation: "jungle")
+
+       expect(@user2.id).to be_present
+    end
+  end
 end
