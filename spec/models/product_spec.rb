5fbabc4c6ec19b6f33d944ab45c04aec744b553b
Isaiah <isaiah.iu@hotmail.com>
2022-05-09 14:52:25 +0000

    First test for Products complete

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
+require 'product'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
+    it 'should save successfully if all validations are met' do
+      @category = Category.new
+      @product = Product.create(name: "test", price: 30, quantity: 6, category: @category)
 
+      expect(@product.id).to be_present
+    end
 
   end
 end
