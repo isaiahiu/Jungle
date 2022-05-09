71b9c436afe069d714f73553d024e51b6108f36b
Isaiah <isaiah.iu@hotmail.com>
2022-05-09 15:18:59 +0000

    Implement rest of tests for validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -9,6 +9,33 @@ RSpec.describe Product, type: :model do
 
       expect(@product.id).to be_present
     end
+    it 'should give an error if name is ommitted' do 
+      @category = Category.new
+      @product = Product.create(name: nil, price: 30, quantity: 6, category: @category)
 
+      expect(@product.errors.full_messages).to include "Name can't be blank"
+      end
+    it 'should give an error if price is ommitted' do 
+      @category = Category.new
+      @product = Product.create(name: "Test", quantity: 6, category: @category)
+
+      expect(@product.id).to_not be_present
+      expect(@product.errors.full_messages).to include "Price cents is not a number", "Price is not a number", "Price can't be blank"
+
+    end
+    it 'should give an error if quantity is ommitted' do 
+      @category = Category.new
+      @product = Product.create(name: "Test", price: 30, quantity: nil, category: @category)
+
+      expect(@product.id).to_not be_present
+      expect(@product.errors.full_messages).to include "Quantity can't be blank"
+    end
+    it 'should give an error if category is ommitted' do 
+      @category = Category.new
+      @product = Product.create(name: "Test", price: 30, quantity: 6, category: nil)
+
+      expect(@product.id).to_not be_present
+      expect(@product.errors.full_messages).to include "Category must exist", "Category can't be blank"
+    end
   end
 end
