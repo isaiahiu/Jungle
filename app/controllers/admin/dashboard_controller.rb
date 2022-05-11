fb6234fdcc6152359e1022fba6d2601f44d861e5
isaiahiu <isaiah.iu@hotmail.com>
2022-05-11 19:50:34 -0400

    Generate Admin::BaseController, method for auth
    - All Admin controllers inherit from Admin::Base, which includes
    the authentication filter.

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,5 @@
-class Admin::DashboardController < ApplicationController
-        http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+class Admin::DashboardController < Admin::BaseController
+
   def show
     @products = Product.all.count
     @categories = Category.count
